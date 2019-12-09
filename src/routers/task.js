25b2e3c4d4cd9fa1ab3585f0e1254e3173ac4284
Fernando Alegría <fernanalegria@gmail.com>
2019-12-09 23:24:14 +0100

    feat: Filter tasks by completed or uncompleted

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,12 +1,14 @@
+const HttpStatus = require("http-status-codes");
 const { getRouter } = require("./crud");
 const { Task } = require("../models");
+const { auth } = require("../middleware");
 
-module.exports = getRouter(
+const taskRouter = getRouter(
   Task,
   {
     create: true,
     readDetail: true,
-    readAll: true,
+    readAll: false,
     update: true,
     delete: true
   },
@@ -14,3 +16,24 @@ module.exports = getRouter(
     allowedUpdates: ["description", "completed"]
   }
 );
+
+taskRouter.get("", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
+  try {
+    await req.user
+      .populate({
+        path: "tasks",
+        match
+      })
+      .execPopulate();
+    res.status(HttpStatus.OK).send(req.user.tasks);
+  } catch (e) {
+    res.status(HttpStatus.SERVICE_UNAVAILABLE).send();
+  }
+});
+
+module.exports = taskRouter;
